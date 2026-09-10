3265c76022c4fc0960cfb0904021754f1f1f696c
Sudhanshu3010 <h.sonare35@gmail.com>
2026-09-10 17:12:05 +0530

    CD updated

---
+++ src/components/Welcoome.jsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+  return (
+    <div>
+      <h1>Welcome to Vite + React!</h1>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
